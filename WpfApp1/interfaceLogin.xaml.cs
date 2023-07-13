5277e6b459ea636231ea88031b11627ae3d21e2d
ony <rabenjarijaonaoa@gmail.com>
2023-07-13 14:24:02 +0300

    miverina @ taniny ny jiosy loginconnect()

--- WpfApp1/interfaceLogin.xaml.cs
+++ WpfApp1/interfaceLogin.xaml.cs
@@ -34,7 +34,7 @@ namespace WpfApp1
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            loginconnect();
         }
         public void loginconnect()
         {
