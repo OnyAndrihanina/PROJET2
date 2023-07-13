154e30e3d7b72e603ac08ff80a3235e9bc59fc47
ony <rabenjarijaonaoa@gmail.com>
2023-07-13 18:05:46 +0300

    afficher fournisseur non validé et validé

--- WpfApp1/Fournisseur.cs
+++ WpfApp1/Fournisseur.cs
@@ -11,16 +11,17 @@ namespace WpfApp1
 {
     internal class Fournisseur
     {
+        
 
-        string nomentreprise { get; set; }
-        string nomproprietaire { get; set; }
-        string adresse { get; set; }
-        string email { get; set; }
-        string nifstat { get; set; }
-        string motdepasse { get; set; }
-        string role { get; set; }
-        bool isfournisseur { get; set; }
-        int solde { get; set; }
+        public string nomentreprise { get; set; }
+        public string nomproprietaire { get; set; }
+        public string adresse { get; set; }
+        public string email { get; set; }
+        public string nifstat { get; set; }
+        public string motdepasse { get; set; }
+        public string role { get; set; }
+        public bool isfournisseur { get; set; }
+        public int solde { get; set; }
 
         //public Fournisseur(string nomentreprise,string nomproprietaire,string adresse,string email,string nifstat,string motdepasse,string role,bool isfournisseur,int solde)
         //{
@@ -44,10 +45,10 @@ namespace WpfApp1
             this.nifstat = nifstat;
         }
 
-        public static ObservableCollection<Fournisseur> chargerDonnees()
+        public static ObservableCollection<Fournisseur> chargerDonneesFournisseurValide()
         {
 
-            string connectionString = "Host=localhost;Port=5432;Database=Energie;Username=postgres;Password=postgres";
+            string connectionString = "Host=localhost;Port=5432;Database=projet2;Username=postgres;Password=ronyandrihanina";
             NpgsqlConnection connection = null;
             NpgsqlCommand command = null;
             NpgsqlDataReader reader = null;
@@ -61,18 +62,12 @@ namespace WpfApp1
                 connection.Open();
 
                 // Préparer la requête SQL pour récupérer les données
-                string query = "SELECT nomentreprise, nomproprietaire, adresse, email, nifstat FROM public.fournisseur where isfournisseur=false;";
+                string query = "SELECT nomentreprise, nompropriétaire, adresse, email, nifstat FROM public.fournisseur where isfournisseur=true;";
                 command = new NpgsqlCommand(query, connection);
 
-                // Exécuter la requête et récupérer les données dans un NpgsqlDataReader
                 reader = command.ExecuteReader();
 
                 Console.WriteLine("Connexion réussi");
-
-                // Créer une liste pour stocker les données récupérées
-                //List<Fournisseur> donnees = new List<Fournisseur>();
-
-
 
                 // Parcourir le NpgsqlDataReader pour récupérer les données
                 while (reader.Read())
@@ -86,22 +81,63 @@ namespace WpfApp1
 
                     Fournisseur frs = new Fournisseur(nomentreprise, nomproprietaire, adresse, mail, nifstat);
 
-                    Console.WriteLine(nomentreprise + " " + nomproprietaire + " " + adresse + " " + mail + " " + nifstat);
 
-                    // Créer un objet VotreClasseDeDonnees avec les valeurs lues
-                    //Fournisseur donnee = new Fournisseur(nomentreprise, nomproprietaire, adresse, mail, nifstat);
-
-                    // Ajouter l'objet à la liste
-                    //donnees.Add(donnee);
                     listeFournisseur.Add(frs);
                 }
-
-                // Assigner la liste comme ItemsSource du DataGrid
 
             }
             catch (Exception ex)
             {
-                // Gérer les exceptions ou les erreurs de connexion
+               
+                MessageBox.Show("Erreur lors de la récupération des données : " + ex.Message);
+            }
+            return listeFournisseur;
+        }
+
+        public static ObservableCollection<Fournisseur> chargerDonneesFournisseurNonValide()
+        {
+
+            string connectionString = "Host=localhost;Port=5432;Database=projet2;Username=postgres;Password=ronyandrihanina";
+            NpgsqlConnection connection = null;
+            NpgsqlCommand command = null;
+            NpgsqlDataReader reader = null;
+            ObservableCollection<Fournisseur> listeFournisseur = new ObservableCollection<Fournisseur>();
+            try
+            {
+                // Créer une nouvelle instance de NpgsqlConnection
+                connection = new NpgsqlConnection(connectionString);
+
+                // Ouvrir explicitement la connexion à la base de données
+                connection.Open();
+                
+                // Préparer la requête SQL pour récupérer les données
+                string query = "SELECT nomentreprise, nompropriétaire, adresse, email, nifstat FROM fournisseur where isfournisseur=false;";
+                command = new NpgsqlCommand(query, connection);
+
+                reader = command.ExecuteReader();
+
+                Console.WriteLine("Connexion réussi");
+
+                // Parcourir le NpgsqlDataReader pour récupérer les données
+                while (reader.Read())
+                {
+                    // Lire les valeurs des colonnes "Nom", "Prenom" et "Adresse"
+                    string nomentreprise = reader.GetString(0);
+                    string nomproprietaire = reader.GetString(1);
+                    string adresse = reader.GetString(2);
+                    string mail = reader.GetString(3);
+                    string nifstat = reader.GetString(4);
+
+                    Fournisseur frs = new Fournisseur(nomentreprise, nomproprietaire, adresse, mail, nifstat);
+
+
+                    listeFournisseur.Add(frs);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
                 MessageBox.Show("Erreur lors de la récupération des données : " + ex.Message);
             }
             return listeFournisseur;
